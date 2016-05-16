37f506acc9d1ca503ebb0dd8dc38c25b87b15d7e
Arun Chandramouli <Arun Chandramouli>
2016-05-16 09:29:41 +0530

    Decorators with Arguments

---
+++ decorators/decoargs.py
@@ -0,0 +1,44 @@
+__author__ = 'Home'
+
+'''
+ Example of Decorator with args
+'''
+
+from functools import wraps, partial
+import logging
+
+def dec_args(prefix=''):
+    def debug(func):
+        logme = logging.getLogger(func.__module__)
+        msg = func.__name__
+        @wraps(func)
+        def wrapper(*args,**kargs):
+            print(prefix + msg)
+            return func(*args,**kargs)
+        return wrapper
+    return debug
+
+
+def debug(func = None , prefix = ''):
+        if func is None:
+            return partial(debug,prefix = prefix)
+        msg = func.__name__
+        @wraps(func)
+        def wrapper(*args,**kargs):
+            print(prefix + msg)
+            return func(*args,**kargs)
+        return wrapper
+
+
+#@dec_args(prefix=' Awe ')
+@debug(prefix='AWE')
+def say_hello():
+    a = 100
+    print(' Hello ' , a)
+
+print say_hello.__class__
+say_hello.__call__()
+print(say_hello.__dict__)
+print(say_hello.func_name)
+print(say_hello.func_code)
+print(say_hello.func_dict)
