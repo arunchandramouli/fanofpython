aa2dd45b83e5ca7cb7fd19bf48a729e671ddbc1d
Arun Chandramouli <Arun Chandramouli>
2016-05-29 22:43:31 +0530

    Sample mutable types

--- builtins/mutables.py
+++ builtins/mutables.py
@@ -67,3 +67,89 @@ assert hash(object1) == hash(object2) ,'\n\n'
 assert id(object1) == id(object2) , '\n\n'
 
 print ' . ' * 50 ,'\n\n'
+
+
+print 'Testing for Mutability - builtin type list!','\n\n'
+
+# ****** Builtin types lists are mutable! ******
+
+listA = range(10)
+#print hash(listA) # lists are not hashable - can't be set as dict key or set item
+
+'''
+	Output:		
+
+		Traceback (most recent call last):
+		  File "mutables.py", line 78, in <module>
+		    print hash(listA)
+		TypeError: unhashable type: 'list'
+'''
+
+listB = listA
+# since they refer same memory location, when listB is modified it will affect listA too... pass by reference!
+listB.extend(range(10,21)) 
+print listA, listB,'\n\n'
+
+# As we could see in the output below both the lists are affected!
+'''
+	Output:
+
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] 
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+'''
+
+# Now check their hashvalue which is same as id()
+
+assert id(listB) == id(listA) , '\n\n' # returns True
+
+del listA # delete listA, but listB will still exist!
+print listB # returns [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print ' . ' * 50 ,'\n\n'
+
+print 'Testing for Mutability - builtin type set!','\n\n'
+
+# ****** Builtin types sets are mutable! but frozensets are immutable ******
+# A Set is an unordered collection of items!
+
+mySet = set()
+print mySet, mySet.__class__
+
+# Are sets hashable?
+
+#print hash(mySet)
+
+'''
+	Traceback (most recent call last):
+	  File "mutables.py", line 121, in <module>
+	    print hash(mySet)
+	TypeError: unhashable type: 'set'
+'''
+
+mySet2 = mySet # Mutable objects
+
+# Now if I perform action on mySet, mySet2 will be updated automatically!
+
+mySet.add('Tim Peters')
+print mySet, mySet2 # returns set(['Tim Peters']) set(['Tim Peters'])
+
+# A mutable object cannot be a set element
+#mySet2.add(range(4))
+
+'''
+	Traceback (most recent call last):
+  	File "mutables.py", line 137, in <module>
+    	mySet2.add(range(4))
+	TypeError: unhashable type: 'list'
+'''
+
+print ' . ' * 50 ,'\n\n'
+
+
+print 'Testing for Mutability - builtin type bytearray!','\n\n'
+
+# ****** Builtin types bytearray are immutable , they can be used as dict keys and set items******
+
+print hash(bytearray) # returns 505762448
+
+print ' . ' * 50 ,'\n\n'
