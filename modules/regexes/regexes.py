ba961c80a18d9717920044f9f5df4e1f5ac301b9
Arun Chandramouli <Arun Chandramouli>
2016-07-01 23:41:48 +0530

    Exploring regexes

--- modules/regexes/regexes.py
+++ modules/regexes/regexes.py
@@ -53,9 +53,13 @@ def re_compile(textString,compile_str):
 	for data in getRegex:
 
 		gets = data.search(textString)
-		if bool(gets):yield data,gets.re.pattern # Yield both pattern and the string
+		
+
+		if bool(gets):
+
+			yield data,gets.re.pattern # Yield both pattern and the string , gets.group() also returns the string
 			
- 	return # return None is legal inside generator
+ 	return # return None is legal inside function that has a generator
 
 getData = re_compile("Arun is in London",["Arun","Kumar"])
 
@@ -95,11 +99,56 @@ def multipleMatches_findIter(pattern,string):
 		yield match object and matched string
 	'''
 
-	for data in re.finditer(pattern, string): yield data,' --- ',data.re.pattern
+	for data in re.finditer(pattern, string): 
+		# yield match object and the match string, data.group() returns match string
+		yield data,' --- ',data.re.pattern
 			
 
 
-gen(multipleMatches_findIter('ab','abbaaabbbbaaaaa'))
+gen(multipleMatches_findIter('ab','abbaaabbbbaaaaa')) # returns value and states "Task Completed!"
+gen(multipleMatches_findIter('12','abbaaabbbbaaaaa')) # just states "Task Completed!"
 
 
 
+'''
+	Forming groups from regular expression match object
+'''
+
+# Step 1 - Compile and get the object
+getObject = re.compile('[a-z]+')
+
+#Explore a compiled re-object
+
+print dir(getObject)
+
+# returns ['__copy__', '__deepcopy__', 'findall', 'finditer', 'match', 'scanner', 'search', 'split', 'sub', 'subn']
+
+# Step 2 - findall :: get as a list 
+
+findallmatches = getObject.findall("abbaaabbbbaaaaa1212abbcdefghy1985jklmnhgfrtyu")
+
+print findallmatches # returns ['abbaaabbbbaaaaa', 'abbcdefghy', 'jklmnhgfrtyu']
+
+# Step 3 - match/search and group
+
+matchGroup = getObject.search("abbaaabbbbaaaaa1212abbcdefghy1985jklmnhgfrtyu")
+
+print matchGroup.group() # returns abbaaabbbbaaaaa
+
+p = re.compile('[a-z]*[0-9]*')
+m = p.search('abcd1212klmnop')
+
+print m.group() # returns abcd1212
+
+# use findall and finditer on compiled re-object
+
+m = p.findall('abcd1212klmnop')
+print m # returns ['abcd1212', 'klmnop', '']
+
+
+m = p.finditer('abcd1212klmnop')
+print m # returns <callable-iterator object at 0x00000000012D1E48>
+matchObject = m.next() # returns <_sre.SRE_Match object at 0x00000000011CA100>
+print dir(matchObject) # returns ['__copy__', '__deepcopy__', 'end', 'expand', 'group', 'groupdict', 'groups', 'span', 'start']
+# Get the match string and the pattern used from the match object
+print matchObject.group(), ' - ', matchObject.re.pattern  # abcd1212  -  [a-z]*[0-9]*
