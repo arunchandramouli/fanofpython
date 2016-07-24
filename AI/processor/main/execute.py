7800e1860c7ed3f7a3bd16ec6ce5953ba0f46490
Arun Chandramouli <Arun Chandramouli>
2016-07-24 23:05:10 +0530

    __main__ defined

---
+++ AI/processor/main/execute.py
@@ -0,0 +1,27 @@
+import engine
+import sys
+import logging
+
+# Append the Path!
+sys.path.append("..\utils")
+import metas
+
+# Set access to engine and metas module
+
+core_engine_logger = logging.getLogger("Core Engine :: ")
+
+setattr(engine,"core_engine_logger",core_engine_logger)
+setattr(engine,"theMetas",metas)
+setattr(metas,"core_engine_logger",core_engine_logger)
+engine.Core_Engine = metas.classdecorator(engine.Core_Engine)
+
+#metas.applyToMultipleAttrs(engine.Core_Engine,['addtoContainer'],metas.pipeline)
+
+def driver(ipfileName,userQuery):
+
+    engine.Core_Engine.execute(ipfileName,userQuery)
+
+# Execution block
+if __name__ == "__main__":
+
+    driver(ipfileName = "..\\input\\support.csv", userQuery= "Set up Visual Studio latest version!")
