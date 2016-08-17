5a352494128239138f6af19c52d6c9e1a7ab1713
Arun Chandramouli <Arun Chandramouli>
2016-08-17 07:49:10 +0530

    Find the most occurring!

--- AI/processor/utils/metas.py
+++ AI/processor/utils/metas.py
@@ -9,6 +9,8 @@ import logging
     This acts as a support to the core engine
 '''
 
+core_engine_logger = None
+
 '''
     A Decorator that can be applied @ class level, to convert all methods as classmethods automatically
 '''
@@ -34,32 +36,51 @@ def applyToMultipleAttrs(klassobject,attrList,whattoapply):
     *** This function takes another function as an input and executes the same.
 '''
 
-def debug(func = None , prefix = ''):
+def pipeline(func = None , prefix = ''):
         if func is None:
             return partial(debug,prefix = prefix)
         msg = func.__name__
 
         core_engine_logger.info("Executing function --- %s "%msg)
-        core_engine_logger.info("/n")
-        core_engine_logger.info("Aim: "%func.__doc__)
-        core_engine_logger.info("/n")
+        core_engine_logger.info("\n")
+        core_engine_logger.info("Aim: %s "%func.__doc__)
+        core_engine_logger.info("\n")
 
-        if not prefix is None:
+        """if prefix.__str__().strip() is not None:                        
             core_engine_logger.info("Category %s"%prefix)
-            core_engine_logger.info("/n")
+            core_engine_logger.info("\n")"""
 
         @wraps(func)
         def wrapper(*args,**kargs):
-            return func(*args,**kargs)
+            theResult = func(*args,**kargs)
+            theResult.next()
+            return theResult
         return wrapper
 
 '''
     A Co-routine function to simulate the pipeline activity inside the engine
 '''
 
-def pipeline(func):
+def execute(func):
 	def theGenerator(*args,**kargs):
 		theResult = func(*args,**kargs)
 		theResult.next()
 		return theResult
 	return theGenerator
+
+
+'''
+    From the given list determine the most occuring
+'''
+
+def most_occurring(inputList):
+
+    loadData = {}
+
+    for items in inputList:
+        
+        if items in loadData.keys():
+            loadData.__setitem__(items,loadData.__getitem__(items)+1)
+        else: loadData.__setitem__(items,1)
+
+    return loadData
