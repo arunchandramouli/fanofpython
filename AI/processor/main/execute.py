c46ec9ce24f14044ad7ff4f03d0b21d9405d065b
Arun Chandramouli <Arun Chandramouli>
2016-08-17 07:50:17 +0530

    Input query

--- AI/processor/main/execute.py
+++ AI/processor/main/execute.py
@@ -1,4 +1,3 @@
-import engine
 
 import sys
 import logging
@@ -7,13 +6,18 @@ import logging
 sys.path.append("..\utils")
 import metas
 
+
 # Set access to engine and metas module
 
 core_engine_logger = logging.getLogger("Core Engine :: ")
+logging.basicConfig(level=logging.INFO)
+setattr(metas,"core_engine_logger",core_engine_logger)
+
+import engine
 
 setattr(engine,"core_engine_logger",core_engine_logger)
 setattr(engine,"theMetas",metas)
-setattr(metas,"core_engine_logger",core_engine_logger)
+
 engine.Core_Engine = metas.classdecorator(engine.Core_Engine)
 
 #metas.applyToMultipleAttrs(engine.Core_Engine,['addtoContainer'],metas.pipeline)
@@ -23,6 +27,14 @@ def driver(ipfileName,userQuery):
     engine.Core_Engine.execute(ipfileName,userQuery)
 
 # Execution block
-if __name__ == "__main__":
+if __name__ == "__main__":   
 
-    driver(ipfileName = "..\\input\\support.csv", userQuery= "Set up Visual Studio latest version")
+   driver(ipfileName = "..\\input\\support.csv", userQuery= "Having connectivity issue in uat")
+
+
+'''
+
+THIS IS REGARDING PROJECT "P0501728 ELENDING AUDIT DATA ARCHIVAL WE ARE HAVING CONNECTIVITY ISSUE IN UAT. 
+PLEASE FIND THE LOGS FOR ERRORS:  HERE IS THE JIRA FOR DETAIL REFERENCE MPS00002-1025 IN CASE REQUIRED  
+
+'''
