0295130d6f3dbe401a7ecfd7c0632ceccdbab294
Arun Chandramouli <Arun Chandramouli>
2016-08-30 14:24:32 +0530

    Source code for main and execute

---
+++ AI/imageprocessing/source/execute.py
@@ -0,0 +1,9 @@
+# import all th required libraries for processing
+
+import main
+
+def runner():
+    print main.image_processing(parentImage = '..\\images\\pancard.jpg',featureImage = '..\\images\\validate.jpg')
+
+if __name__ == "__main__":
+    runner()
