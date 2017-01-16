50970041a004f53ea46d71be1b77a9e9e2c7ffb0
Arun Chandramouli <Arun Chandramouli>
2017-01-16 10:15:42 +0530

    'tests'

--- research/excel/ws.py
+++ research/excel/ws.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import datetime
+import time
 from lxml import html
 
 geturl = 'https://www.wayfair.com/careers'
@@ -24,22 +24,30 @@ job_post_loc = "//*[@class='jobCat pos_rel margin_lg_bottom']//*[contains(@class
 
 
 print 'Driver'
+millis = int(round(time.time() * 1000))
 driver = webdriver.PhantomJS('C:/PhantomJs/bin/phantomjs')
-print 'Driver-1'
-x = datetime.datetime.now()
-driver.get(geturl)
+millis1 = int(round(time.time() * 1000))
 
-print "TIME ==== ", x - datetime.datetime.now()
+print 'Driver-1', millis1 - millis
+
+millis = int(round(time.time() * 1000))
+driver.get(geturl)
+millis1 = int(round(time.time() * 1000))
+print "TIME 1 ==== ", millis1 - millis
 
 print driver.current_url
 
-x = datetime.datetime.now()
+millis = int(round(time.time() * 1000))
 element = WebDriverWait(driver, 20).until(
     EC.presence_of_element_located((By.ID, "bd")))
 
+millis1 = int(round(time.time() * 1000))
 print 'Done'
 
-print "TIME ==== ", x - datetime.datetime.now()
+print "TIME 2 ==== ", millis1 - millis
+
+
+driver.execute_script("document.getElementById('careers_opportunities').style.display = 'career_opportunities_content careers_tab'")
 
 with open("tests.html",'w') as writer:
 	writer.write(driver.page_source.encode('utf-8'))
