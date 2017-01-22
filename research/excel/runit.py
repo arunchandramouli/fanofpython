61e25a14f17650c926d18789c68f61473a390505
Arun Chandramouli <Arun Chandramouli>
2017-01-22 23:54:30 +0530

    tests

--- research/excel/runit.py
+++ research/excel/runit.py
@@ -1,6 +1,6 @@
 
 
-
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -35,47 +35,57 @@ weekly_price_value = "//*[contains(@class,'pricingarea')]//*[@id='week']//b/text
 monthly_price_currency = "//*[contains(@class,'pricingarea')]//*[@id='month']//strong/text()"
 monthly_price_value = "//*[contains(@class,'pricingarea')]//*[@id='month']//b/text()"
 
-zipcode_enter = ".//*[@id='newzipcodecart']"
-zipcode_enter_submit = ".//*[@class='newzipCart']"
-zipcode_enter_click = "//*[@class='editBranch']//p[@class='sb-note-txt']"
+zipcode_enter = "//*[@class='row']//*[@class='editBranch']//*[@class='sb-note-txt']"
+modal_dialog_wait = "//*[@class='newzipCart']"
+
+zipcode_enter_submit = "//*[@id='myModalCart']//*[@class='modal-dialog']//*[@class='modal-content']//*[@class='form-group']//*[@class='newzipCart']"
+zipcode_enter_click = "//*[@id='myModalCart']//*[@class='modal-dialog']//*[@class='modal-content']//*[@class='form-group']//*[@id='newzipcodecart']"
+
+test_zipcodes = ["77022","75201","30303","10022","10005"]
+
 
 optionschrome = webdriver.ChromeOptions()
 optionschrome.add_argument("--start-maximized")
 
 
-def driver_window_handles(driver):
-
-	print "Window"
-	#driver.find_element_by_xpath(zipcode_enter_click).click()
-	time.sleep(3)
-	driver.save_screenshot("True"+".png")
 
 
-	driver.switch_to_window(driver.window_handles[0])	
-	time.sleep(3)
-	print True
-
-	driver.find_element_by_xpath(zipcode_enter_click).send_keys("10005")
-	driver.find_element_by_xpath(zipcode_enter_submit).click()
-	return
+def driver_window_handles(driver,zipcode_enter_field):
 
 
+		try:
+			driver.find_element_by_xpath(zipcode_enter).click()
+		except Exception:
+			pass
 
+		time.sleep(7)
+		print driver.save_screenshot("tests1.png")
+		driver.switch_to_window(driver.window_handles[0])	
+		#driver.switch_to_alert()
+		
+		print driver.save_screenshot("tests.png")
+		element = WebDriverWait(driver, 80).until(
+		EC.visibility_of_element_located((By.XPATH, modal_dialog_wait)))
+
+		hov = ActionChains(driver).move_to_element(element)
+		hov.perform()
+
+		driver.find_element_by_xpath(zipcode_enter_click).click()
+		driver.find_element_by_xpath(zipcode_enter_click).send_keys(zipcode_enter_field)
+		driver.find_element_by_xpath(zipcode_enter_submit).click()
+		return
 
 print geturl
 
-#driver = webdriver.PhantomJS('C:/PhantomJs/bin/phantom2/phantomjs')
-driver = webdriver.Chrome('chromedriver',chrome_options=optionschrome)
+driver = webdriver.PhantomJS('C:/PhantomJs/bin/phantom2/phantomjs')
+#driver = webdriver.Chrome('C:/Users/Home/Documents/chromedriver',chrome_options=optionschrome)
 
-print driver
 
 driver.get(geturl)
 
-print driver.current_url
 
-element = WebDriverWait(driver, 30).until(
+element = WebDriverWait(driver, 80).until(
     EC.presence_of_element_located((By.XPATH, wait_xpath)))
-
 
 
 source = html.fromstring(driver.page_source.encode("utf-8"))
@@ -87,47 +97,48 @@ for elements in source.xpath(all_xpath):
 	element = WebDriverWait(driver, 30).until(
     EC.presence_of_element_located((By.XPATH, wait_xpath)))
 
-	print driver.current_url
+	equiment_type = elements.split("/")[-1].strip()
 
 	'''
 		Get Categories URL
 	'''
 	source = html.fromstring(driver.page_source.encode("utf-8"))
 	for elementscats in source.xpath(categories):
+
+		elementscats_sub = elementscats.split("/")[-1].strip()
+
 		get_prd_url = base_url+elementscats
 
 		driver.get(get_prd_url)
 		element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, wait_xpath)))
 
-		print driver.current_url
+		for each_zip_code in test_zipcodes:
 
-		driver_window_handles(driver)
+			driver_window_handles(driver)
 
-		element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, wait_xpath)))
+			element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, wait_xpath)))
 
-		source = html.fromstring(driver.page_source.encode("utf-8"))
+			source = html.fromstring(driver.page_source.encode("utf-8"))
 
-		''' Find all Sub-Categories options'''
-		for subcats in source.xpath(subcats):
-			
-			#Click on each element and find the price details
+			''' Find all Sub-Categories options'''
+			for subcats in source.xpath(subcats):
+				
+				#Click on each element and find the price details
 
-			if not subcats == "Select Equipment":
-			
-				driver.find_element_by_xpath(select_subcats%subcats).click()
+				if not subcats == "Select Equipment":
+				
+					driver.find_element_by_xpath(select_subcats%subcats).click()
 
-				'''
-					Get Sub-Categories List
-				'''
+					'''
+						Get Sub-Categories List
+					'''
 
-				# Wait for the Pricing Area div to load
-				element = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, pricing_area_wait)))
+					# Wait for the Pricing Area div to load
+					element = WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, pricing_area_wait)))
+					time.sleep(1)
+					source = html.fromstring(driver.page_source.encode("utf-8"))
+					time.sleep(1)
 
-				print 'Select'
-				time.sleep(3)
-				source = html.fromstring(driver.page_source.encode("utf-8"))
-				time.sleep(3)
-
-				print source.xpath(daily_price_value),source.xpath(daily_price_currency)
-				print source.xpath(weekly_price_value),source.xpath(weekly_price_currency)
-				print source.xpath(monthly_price_value),source.xpath(monthly_price_currency)
+					print equiment_type, elementscats_sub,subcats, each_zip_code, source.xpath(daily_price_value),source.xpath(daily_price_currency)
+					print equiment_type, elementscats_sub,subcats,each_zip_code, source.xpath(weekly_price_value),source.xpath(weekly_price_currency)
+					print equiment_type, elementscats_sub,subcats,each_zip_code, source.xpath(monthly_price_value),source.xpath(monthly_price_currency)
