395691aa574cac2a75f0480c177c6c2a73a8e2f0
Arun Chandramouli <Arun Chandramouli>
2017-01-23 09:20:31 +0530

    tests

--- research/excel/run.py
+++ research/excel/run.py
@@ -3,6 +3,7 @@ import re
 import calendar
 
 _values_to_eliminate =['%Change','Thousand Metric','Month','Date']
+valid_sheets = ["T1","T2","T3","T4","T5","T6","T7","T8"]
 '''
  S1 : Check for headers to be present in various rows in the file, if yes, split by the headers
  S2 : Check for Sub-Tables inside the main table eg : Sheet-T8 , oil1114.xlsx
@@ -12,136 +13,139 @@ def read_excel(excelfull_path):
 
 
 	sheet = openpyxl.load_workbook(excelfull_path)
-	ws = sheet.get_sheet_by_name('T8')	
-	
-	'''
-		S0: Load all the Sheet Values into a Container for Processing
-	'''
-	get_rows_val = (load_values_container(container = [] , iter_rows =ws.iter_rows()))
 
-	'''
-		S1: Fetch the Header information from the Tables
-	'''
+	for sheets_by_name in valid_sheets:
 
-	header_value , mx_mn ,len_max , len_min, previous_row,next_row =  determine_headers(get_rows_val)
-	original_header_value = str(''.join(header_value))
-
-	'''
-		S2: Modify the Header to accept missing cells such as one below the header
-		This function will get executed only if there are any such identifiers below the headers that
-		are neither of len_max or close to it..
-
-		For eg ::
-
-			2012	2013	1Q2013	2Q2013	3Q2013	4Q2013	Jan2014	Current	Year to -> Header
-																	Month1	Date2 -> Cells belonging to Header
-		Function will add those cells to the Header														
-	'''
-	
-	modified_header_value = modify_header(header_value , mx_mn ,len_max , len_min, previous_row,next_row)
-
-	
-	'''
-		S2: Iterate over the entire sheet and modify all headers as modified_header_value
-	'''
-
-	modified_rows = modify_table_rows(get_rows_val,original_header_value,modified_header_value)
-
-	modified_rows = eliminate_values_container(modified_rows,modified_header_value)
-	
-
-	'''
-		S1 : Headers are present in more than one location  - Refer Sheet T3 , File -> oil1114.xlsx
-
-		OECD Americas: Imports of Crude Oil, NGL and Refinery Feedstocks 1							
-							Thousand metric tons
-		Imports from:	2012	2013	1Q2013	2Q2013	3Q2013	4Q2013	Jan2014 --> Header
-		Australia	274	143	2	65	76	-	-
-		Belgium	2017	2362	501	514	770	577	164
-		Canada	127017	135706	34998	31743	33658	35307	12955		
-		Denmark	7	62	56	6	-	-	-
-
-		OECD Asia Oceania: Imports of Crude Oil, NGL and Refinery Feedstocks 1							
-							Thousand metric tons
-		Imports from:	2012	2013	1Q2013	2Q2013	3Q2013	4Q2013	Jan2014
-		Australia	3097	3416	769	985	708	954	115
-		Belgium	-	-	-	-	-	-	-
-		Canada	-	-	-	-	-	-	-
-		Denmark	-	-	-	-	-	-	-
-		France	-	-	-	-	-	-	-
-
-	'''
-	
-	get_list_headers_location = check_headers_in_file(modified_header_value,modified_rows)
-
-
-
-	'''
-		S2 : Check for Sub-Tables - Refer Sheet T8 , File -> oil1114.xlsx
-
-		The Sheet has only one header but the rows are split by keys representing values beneath
-		and all the keys belong to the same table and header
-
-											%Change	%Change
-			2012	2013	1Q2013	2Q2013	3Q2013	4Q2013	Jan2014	Current	Year to **** --> Header **** 
-									Month1	Date2
-		OECD Americas **** --> Key **** 
+		ws = sheet.get_sheet_by_name(sheets_by_name)	
 		
-		Liquefied Petroleum Gases	20978	20805	4185	7130	6587	2903	1319	16.5	16.5
-		Naphtha	15530	16839	4026	4217	4395	4201	1466	1.2	1.2
-		Total Gasoline3	431393	442597	105054	110542	112624	114377	37072	3.7	3.7
+		'''
+			S0: Load all the Sheet Values into a Container for Processing
+		'''
+		get_rows_val = (load_values_container(container = [] , iter_rows =ws.iter_rows()))
+
+		'''
+			S1: Fetch the Header information from the Tables
+		'''
+
+		header_value , mx_mn ,len_max , len_min, previous_row,next_row =  determine_headers(get_rows_val)
+		original_header_value = str(''.join(header_value))
+
+		'''
+			S2: Modify the Header to accept missing cells such as one below the header
+			This function will get executed only if there are any such identifiers below the headers that
+			are neither of len_max or close to it..
+
+			For eg ::
+
+				2012	2013	1Q2013	2Q2013	3Q2013	4Q2013	Jan2014	Current	Year to -> Header
+																		Month1	Date2 -> Cells belonging to Header
+			Function will add those cells to the Header														
+		'''
 		
+		modified_header_value = modify_header(header_value , mx_mn ,len_max , len_min, previous_row,next_row)
+
 		
-		OECD Asia Oceania **** --> Key **** 
-		Liquefied Petroleum Gases	6967	7527	1817	1929	2048	1733	662	7.6	7.6
-		Naphtha	38992	40102	10535	9174	10334	10059	3377	-7.5	-7.5
-		Total Gasoline3	70715	70529	17432	16850	17951	18296	6056	-0.1	-0.1
+		'''
+			S2: Iterate over the entire sheet and modify all headers as modified_header_value
+		'''
 
-	'''
+		modified_rows = modify_table_rows(get_rows_val,original_header_value,modified_header_value)
 
-	#presence_of_sub_tables = check_presence_of_sub_tables(get_rows_val,len_max , len_min,header_value)
+		modified_rows = eliminate_values_container(modified_rows,modified_header_value)
+		
 
-	'''
-		Prepare data for the final table
-	'''
+		'''
+			S1 : Headers are present in more than one location  - Refer Sheet T3 , File -> oil1114.xlsx
 
-	mapping = prepare_data_final_table(get_list_headers_location,modified_rows,modified_header_value)
+			OECD Americas: Imports of Crude Oil, NGL and Refinery Feedstocks 1							
+								Thousand metric tons
+			Imports from:	2012	2013	1Q2013	2Q2013	3Q2013	4Q2013	Jan2014 --> Header
+			Australia	274	143	2	65	76	-	-
+			Belgium	2017	2362	501	514	770	577	164
+			Canada	127017	135706	34998	31743	33658	35307	12955		
+			Denmark	7	62	56	6	-	-	-
+
+			OECD Asia Oceania: Imports of Crude Oil, NGL and Refinery Feedstocks 1							
+								Thousand metric tons
+			Imports from:	2012	2013	1Q2013	2Q2013	3Q2013	4Q2013	Jan2014
+			Australia	3097	3416	769	985	708	954	115
+			Belgium	-	-	-	-	-	-	-
+			Canada	-	-	-	-	-	-	-
+			Denmark	-	-	-	-	-	-	-
+			France	-	-	-	-	-	-	-
+
+		'''
+		
+		get_list_headers_location = check_headers_in_file(modified_header_value,modified_rows)
 
 
 
-	'''
-		Check if the tables have divisions - A table under same header has divisions for each region
+		'''
+			S2 : Check for Sub-Tables - Refer Sheet T8 , File -> oil1114.xlsx
 
-		Eg ::
+			The Sheet has only one header but the rows are split by keys representing values beneath
+			and all the keys belong to the same table and header
 
 												%Change	%Change
-			2012	2013	1Q2013	2Q2013	3Q2013	4Q2013	Jan2014	Current	Year to ---> Header
+				2012	2013	1Q2013	2Q2013	3Q2013	4Q2013	Jan2014	Current	Year to **** --> Header **** 
 										Month1	Date2
-			Switzerland	 --> Division 1
-			Liquefied Petroleum Gases	172	175	52	43	43	37	15	-16.7	-16.7
-			Naphtha	13	5	1	1	1	2	-	-100.0	-100.0
-			Total Gasoline3	2930	2804	652	718	733	701	212	-1.9	-1.9
+			OECD Americas **** --> Key **** 
+			
+			Liquefied Petroleum Gases	20978	20805	4185	7130	6587	2903	1319	16.5	16.5
+			Naphtha	15530	16839	4026	4217	4395	4201	1466	1.2	1.2
+			Total Gasoline3	431393	442597	105054	110542	112624	114377	37072	3.7	3.7
+			
+			
+			OECD Asia Oceania **** --> Key **** 
+			Liquefied Petroleum Gases	6967	7527	1817	1929	2048	1733	662	7.6	7.6
+			Naphtha	38992	40102	10535	9174	10334	10059	3377	-7.5	-7.5
+			Total Gasoline3	70715	70529	17432	16850	17951	18296	6056	-0.1	-0.1
 
-			Turkey --> Division 2
-			Liquefied Petroleum Gases	3622	3734	805	931	1039	959	244	-9.0	-9.0
-			Naphtha	1675	756	151	137	188	280	76	43.4	43.4
-			Total Gasoline3	1038	1287	333	330	362	262	52	-61.5	-61.5
+		'''
+
+		#presence_of_sub_tables = check_presence_of_sub_tables(get_rows_val,len_max , len_min,header_value)
+
+		'''
+			Prepare data for the final table
+		'''
+
+		mapping = prepare_data_final_table(get_list_headers_location,modified_rows,modified_header_value)
 
 
-	'''
 
-	are_divisions_exist = bool(check_for_divisions_table(mapping))
+		'''
+			Check if the tables have divisions - A table under same header has divisions for each region
 
-	# IF the Tables don't have divisions, call the final table loader directly.
+			Eg ::
 
-	if not bool(are_divisions_exist):
+													%Change	%Change
+				2012	2013	1Q2013	2Q2013	3Q2013	4Q2013	Jan2014	Current	Year to ---> Header
+											Month1	Date2
+				Switzerland	 --> Division 1
+				Liquefied Petroleum Gases	172	175	52	43	43	37	15	-16.7	-16.7
+				Naphtha	13	5	1	1	1	2	-	-100.0	-100.0
+				Total Gasoline3	2930	2804	652	718	733	701	212	-1.9	-1.9
 
-			# For the Tables in the Container
-			for table_name, table_content in mapping:
+				Turkey --> Division 2
+				Liquefied Petroleum Gases	3622	3734	805	931	1039	959	244	-9.0	-9.0
+				Naphtha	1675	756	151	137	188	280	76	43.4	43.4
+				Total Gasoline3	1038	1287	333	330	362	262	52	-61.5	-61.5
 
-				# For the final table
-				filtered_table_content = [items for items in table_content if len(items) == len_max]
-				design_table(table_name = table_name,table_headers = table_content[0],table_content=filtered_table_content)
+
+		'''
+
+		are_divisions_exist = bool(check_for_divisions_table(mapping))
+
+		# IF the Tables don't have divisions, call the final table loader directly.
+
+		if not bool(are_divisions_exist):
+
+				# For the Tables in the Container
+				for table_name, table_content in mapping:
+
+					# For the final table
+					filtered_table_content = [items for items in table_content if len(items) == len_max]
+					design_table(table_name = table_name,table_headers = table_content[0],table_content=filtered_table_content)
 	
 
 
