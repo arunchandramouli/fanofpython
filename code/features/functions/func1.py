e3ae3f579f7cf52855022bb123fc82024197a863
Arun Chandramouli <Arun Chandramouli>
2017-02-07 09:33:34 +0530

    functions

--- code/features/functions/func1.py
+++ code/features/functions/func1.py
@@ -25,8 +25,9 @@ if __name__ == "__main__":
 	
 	#calculate(10,20) # This will result in an error since function calcualte has 3 parameters
 
-	get_func = calculate(10,20)
-	print get_func
+	# Here we will pass value of x,y alone
+	get_func = partial(calculate,x = 10, y = 20)
+	print get_func(z=10) # Returns 210
 
 	# ************************************************************************************** #
 
