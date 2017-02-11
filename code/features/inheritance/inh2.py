f4c3790a521707cb42818262ac2f8630203ba620
Arun Chandramouli <Arun Chandramouli>
2017-02-11 13:25:23 +0530

    Concept of Inheritance - Explore

--- code/features/inheritance/inh2.py
+++ code/features/inheritance/inh2.py
@@ -1,6 +1,6 @@
-
 '''
     Use Case: Inherit the Base Class and access it's attributes
+
 
     ***** Please be informed that if we dont use initialize the base classes , we shall not have direct access to it's 
     instance specific attributes. *****   
@@ -8,6 +8,7 @@
     Note : Instance specific attributes are those that are initialized inside the __init__ block.
 
 '''
+
 
 
 
@@ -46,19 +47,24 @@
 
 
 '''
-    Use Case: A Class that inherits only one base class - Single Inheritance
+    Use Case: A Class that inherits multiple base classes- Multiple Inheritance
 
 '''
+
+''' Define Class Person and it's methods and attributes '''
 
 class Person(object):
 
     '''
         Person Class contains 2 instance level attributes namely fName and lName and returns the same
-        in the instance method - state_name.
+        in the instance method - state_details.
 
         ***** The instance specific attributes ae available only for it's instances
         and accessible via instance methods naturally. It can still be made available to other parts of the class but the Programmer has
         to explicitly write some code ****
+
+
+        Class Person is the base class, it shall be inherited by it's subclasses
 
     '''
 
@@ -68,17 +74,57 @@ class Person(object):
             Initialize the First and Last name
 
             * * * These are instance specific attributes * * *
-
         '''
         
         self.fName = first
         self.lName = last
 
-    def state_name(self):
+    
+    def state_details(self):
+    
         '''
             Concatenate and return the first and last name
         '''
         return self.fName + " " + self.lName
+
+
+'''
+    Office Class contains 2 instance level attributes namely cabinID and empCategory and returns the same
+    in the instance method - state_details.
+
+    ***** The instance specific attributes are available only for it's instances
+    and accessible via instance methods naturally. It can still be made available to other parts of the class but the Programmer has
+    to explicitly write some code ****
+
+
+    Class Person is the base class, it shall be inherited by it's subclasses
+
+'''
+
+
+''' Define Class Office and it's methods and attributes '''
+
+class Office(object):
+
+    def __init__(self, cabin, ecats):
+
+        '''
+            Initialize the Cabin ID and Employee Category
+
+            * * * These are instance specific attributes * * *
+        '''
+
+        self.cabinID = cabin
+        self.empCategory = ecats
+
+
+    def state_details(self):
+        
+        '''
+            Return the cabin id and employee category
+        '''
+
+        return (self.cabinID,self.empCategory)
 
 
 
@@ -86,7 +132,9 @@ class Person(object):
     Use Case: Employee Class below inherits Person and shall have access to all of its attributes
 '''
 
-class Employee(Person):
+''' Define Class Employee and it's methods and attributes '''
+
+class Employee(Person,Office):
 
     ''' Initialize Employee Class '''
 
@@ -104,18 +152,17 @@ class Employee(Person):
 
             We can thus access them as self.fName, self.lName ....
 
-            * * * These are instance specific attributes * * *
-
         '''
 
         super(Employee,self).__init__(fName,lName)
 
 
-    def state_name(self):
+    def state_details(self):
 
         return "Friend - Employee Class"
 
-    def get_name(self):
+
+    def get_details(self):
 
         '''
             Invoke the method Name in the Super Class
@@ -123,19 +170,49 @@ class Employee(Person):
 
         ''' ***** Please understand the difference carefully between the method calls below ***** '''
 
-        ''' Will call state_name method, will initially search in this class(Employee), if not found will refer to Parent Class '''
-        
-        print "Hello " + self.state_name()
-
         ''' 
-            Will call state_name method, will ***** not initially search ***** in this class(Employee), 
+
+            Will call state_details method, will initially search in this class(Employee), if not found will refer to Parent Class .
+
+            Since we have state_details method in the Employee Class , only that will get executed .
+
+        '''
+        
+        print "Hello " + self.state_details()
+
+
+        
+        ''' 
+            Will call state_details method, will ***** not initially search ***** in this class(Employee), 
             it will refer to Parent Class directly since we have used super()
+
+            We have 2 super(parent) classes and both of it define state_details method, but the first in our order is
+
+            Person, which is defined Employee(Person,Office) which means that state_details of Person Class will get called .
+
+            It follows the order known as mro(Method Resolution Order). It will search all parents and their ancestors in order
+
+            until the required method is found at some point .
+
+            If it's not there with any of the Parents , then an exception shall be raised (AttributeError).
+
         '''
 
-        print "Hello " + super(Employee,self).state_name() # The method in the class Person will get invoked!
+        print "Hello " + super(Employee,self).state_details() # The method in the class Person will get invoked!
+
+
 
 
 ''' Create an instance of class Employee '''
 
 class_emp = Employee("Allan","Donald")
-class_emp.get_name()
+class_emp.get_details()
+
+
+''' Print all the parent classes of Employee (Also called as base classes) '''
+
+
+print "-"*50,'\n'
+
+print "Base Classes of Employee is/ are - ",(Employee.__bases__)
+print "-"*50,'\n'
---
+++ code/features/inheritance/inh3.py
@@ -0,0 +1,257 @@
+'''
+    Use Case: Inherit the Base Class and access it's attributes
+
+    In this example, we would not use __init__ to initialize the base classes, but we shall
+    be accessing the attributes of the parent class using an instance.
+
+
+    ***** Please be informed that if we dont use initialize the base classes , we shall not have direct access to it's 
+    instance specific attributes. *****
+   
+
+    Note : Instance specific attributes are those that are initialized inside the __init__ block.
+
+
+'''
+
+
+
+'''
+
+    About mro - Method Resolution Order 
+
+    A short note an how mro works in realtime
+
+    Say we have a class - class Xyx(A,B,C,X,D,F,G)
+
+    We have a class Xyx that has 7 parent classes namely (A,B,C,X,D,F,G)
+
+    When we create an instance and access any method this is what happens;
+
+    It first searches in the class Xyx to confirm if the method is present there, if found return and break
+
+    Else it begins to search the parent classes(super classes)
+
+    It will start as per the ranking, here we have the ranking as (A,B,C,X,D,F,G)
+
+    It will first search for the method in class A, if found return and break, else it now checks if class A
+    has got any Parent Classes, if yes, it will search all of it's parent classes, if found anywhere return and break .
+
+    Else it goes to class B, it will search for the method in class B , if found return and break, else it now checks if class B
+    has got any Parent Classes, if yes, it will search all of it's parent classes, if found anywhere return and break .
+
+    The same procedure is done for all of the Parent Classes until the method is found.
+
+    In any case if the method is not found any where it raises an AttributeError , meaning that method name is not to be
+    found anywhere in the hierarchy.
+     
+
+'''
+
+
+'''
+    Use Case: A Class that inherits multiple base classes- Multiple Inheritance
+
+'''
+
+
+''' Define Class Alpha and it's methods and attributes '''
+
+class Alpha(object):
+
+    '''
+        Class Alpha is a base class and it shall be inherited by its subclass!
+
+        It has 3 methods namely method1, methodx, method2
+    '''
+
+
+    '''
+        Alpha Class - method1
+    '''
+
+    def method1(self):
+
+        return "Method-1 from Class Alpha"
+
+
+    '''
+        Alpha Class - method2
+    '''
+
+    def method2(self):
+
+        return "Method-2 from Class Alpha"
+
+
+    '''
+        Alpha Class - methodx
+    '''
+
+    def methodx(self):
+
+        return "Method-X from Class Alpha"
+
+
+
+''' Define Class Beta and it's methods and attributes '''
+
+class Beta(object):
+
+    '''
+        Class Beta is a base class and it shall be inherited by its subclass!
+
+        It has 4 methods namely method1, methodx, method2, methodb
+    '''
+
+
+    '''
+        Beta Class - methodb1
+    '''
+
+    def methodb1(self):
+
+        return "Method-b1 from Class Beta"
+
+
+    '''
+        Beta Class - methodb2
+    '''
+
+    def methodb2(self):
+
+        return "Method-b2 from Class Beta"
+
+
+    '''
+        Beta Class - methodbx
+    '''
+
+    def methodbx(self):
+
+        return "Method-bX from Class Beta"
+
+
+
+    '''
+        Beta Class - methodb
+    '''
+
+    def methodd(self):
+
+        return "Method-d from Class Beta"
+
+
+
+
+
+
+''' Define Class Gamma and it's methods and attributes '''
+
+class Gamma(object):
+
+    '''
+        Class Gamma is a base class and it shall be inherited by its subclass!
+
+        It has 4 methods namely method1, method3, methodq, methodw
+    '''
+
+
+    '''
+        Gamma Class - method1
+    '''
+
+    def method1(self):
+
+        return "Method-1 from Class Gamma"
+
+
+    '''
+        Gamma Class - method3
+    '''
+
+    def method3(self):
+
+        return "Method-3 from Class Gamma"
+
+
+    '''
+        Gamma Class - methodq
+    '''
+
+    def methodq(self):
+
+        return "Method-Q from Class Gamma"
+
+    '''
+        Gamma Class - methodw
+    '''
+
+    def methodw(self):
+
+        return "Method-W from Class Gamma"
+
+
+
+''' Define a Sub-class by name Sub_Class and let it inherit Alpha, Beta, Gamma '''
+
+class Sub_Class(Alpha,Beta,Gamma):
+
+
+    '''
+        Class Sub_Class inherits 3 parent classes namely Alpha,Beta,Gamma and thus has access to all of its methods and attributes.
+
+        It has also got a method on it's own , say method_sub which returns "method_sub from class Sub_Class"
+
+        Now When we create an instance of class Sub_Class , and access the methods of the Parents, it goes in an order
+
+        and searches all the parent classes.
+        
+        Let's consider an example
+    
+        ******  Say we want to access methodw *****
+
+
+        Now, Search begins at Class Sub_Class (if the method is found, it breaks here) if not then it goes to 
+
+        Alpha and all of it's parents (if the method is found, it breaks here) ... 
+
+        if still not then it goes to Beta and all of it's parents (if the method is found, it breaks here).. and if still not found it goes 
+
+        to Gamma and all of it's parents (if the method is found, it breaks here).
+
+        This concept is known as mro(Method Resolution Order)
+
+        In any case if the method is not found in it's own class or any of it's parent's it raises an AttributeError
+
+    '''
+
+
+    ''' Define method method_sub '''
+
+    def method_sub(self):
+
+        return "Method method_sub from Class Sub_Class"
+
+
+
+
+''' Now let us go-ahead and create an instance of class Sub_Class '''        
+
+instance_of_class_sub_class = Sub_Class()
+
+''' Now let us begin to access all the methods one by one '''
+
+print "-"*50,"\n\n"
+print instance_of_class_sub_class.method1(),"\n\n"
+print instance_of_class_sub_class.method2(),"\n\n"
+print instance_of_class_sub_class.method3(),"\n\n"
+print instance_of_class_sub_class.methodx(),"\n\n"
+print instance_of_class_sub_class.methodw(),"\n\n"
+print instance_of_class_sub_class.methodq(),"\n\n"
+print instance_of_class_sub_class.methodd(),"\n\n"
+print instance_of_class_sub_class.methodb1(),"\n\n"
+print instance_of_class_sub_class.methodb2(),"\n\n"
+print instance_of_class_sub_class.methodbx(),"\n\n"
+print instance_of_class_sub_class.method_sub(),"\n\n"
+print "-"*50,"\n\n"
