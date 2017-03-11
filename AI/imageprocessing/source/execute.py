57835a52a4f455544a7c53c4585ae21509172e0c
Arun Chandramouli <Arun Chandramouli>
2017-03-11 17:26:59 +0530

    Clean-up

--- AI/imageprocessing/source/execute.py
+++
@@ -1,9 +0,0 @@
-# import all th required libraries for processing
-
-import main
-
-def runner():
-    print main.image_processing(parentImage = '..\\images\\pancard.jpg',featureImage = '..\\images\\validate.jpg')
-
-if __name__ == "__main__":
-    runner()
