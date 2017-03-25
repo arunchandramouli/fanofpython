fd88ea9096869d182df43dceaf77572ee7b4015f
Arun Chandramouli <Arun Chandramouli>
2017-03-25 09:33:39 +0530

    __contains

--- code/features/objects/get2.py
+++ code/features/objects/get2.py
@@ -1,4 +1,6 @@
 
+import re
+
 '''
 	Aim :: Express various object types in python
 
@@ -53,3 +55,47 @@ saylove = love()
 
 
 print hasattr(alist,"__hash__")
+
+
+
+'''
+	Check for contains
+'''
+somelist = ["Robert De Niro","Angelina Jolie","Brad Pitt","Emma Watson","Roger Federer","Annabelle"]
+
+'''
+	Check for a substring Ang in that lst
+'''
+
+'''
+	Using __contains__
+'''
+for items in somelist:
+
+	if items.__contains__("Ang"):
+
+		print items
+
+	if "Ang" in str(items):
+
+		print items
+
+
+'''
+	Using RE
+'''
+for items in somelist:
+
+	''' 
+		re.search -> This will return True if substring Ang is present 
+		in any part of the string 
+		Say it return True for Anglais as well as Language - We will convert both to lower
+	'''
+	if re.search("son".lower(),items.lower()):
+
+		print items
+
+	if re.match("Em".lower(),items.lower()):
+
+		print items
+
